d729c794c639312926bfbcb6270eea3dede90746
Amir Famili <44036070+AmirFamili@users.noreply.github.com>
2025-10-27 23:01:38 -0500

    comment whats app and stackoverflow

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
                 <i className="text-3xl p-3 fa-brands fa-github"></i>
             </a>
 
-            <a href="https://stackoverflow.com/users/22900969/amir-famili" target="_blank" rel="noreferrer">
+            {/* <a href="https://stackoverflow.com/users/22900969/amir-famili" target="_blank" rel="noreferrer">
                 <i className="text-3xl p-3 fa-brands fa-stack-overflow"></i>
             </a>
 
             <a href="https://wa.me/09125835269" target="_blank" rel="noreferrer">
                 <i className="text-3xl p-3 fa-brands fa-whatsapp"></i>
-            </a>
+            </a> */}
 
             <a href="https://t.me/amirm222" target="_blank" rel="noreferrer">
                 <i className="text-3xl p-3 fa-brands fa-telegram"></i>
